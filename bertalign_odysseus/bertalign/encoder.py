47d78d1d11df70bd3ae8d5ddc8bdbe0ff71ec0ec
OdysseusPolymetis <odysseuspolymetis2010@gmail.com>
2026-03-16 21:59:09 +0100

    Update encoder.py

--- bertalign_odysseus/bertalign/encoder.py
+++ bertalign_odysseus/bertalign/encoder.py
@@ -1,34 +1,33 @@
 import numpy as np
-
 from sentence_transformers import SentenceTransformer
 from bertalign.utils import yield_overlaps
 
 class Encoder:
-    def __init__(self, model_name, device='cuda', batch_size=64, show_bar=False):
-        self.model = SentenceTransformer(model_name)
-        self.model_name = model_name
+    def __init__(self, model_name_or_path, device='cuda', batch_size=64, show_bar=False):
+        self.model_name = model_name_or_path
         self.device = device
         self.batch_size = batch_size
         self.show_bar = show_bar
+
+        self.model = SentenceTransformer(model_name_or_path, device=device)
 
     def transform(self, sents, num_overlaps):
         overlaps = []
         for line in yield_overlaps(sents, num_overlaps):
             overlaps.append(line)
 
-        # Batch + GPU
         sent_vecs = self.model.encode(
             overlaps,
             device=self.device,
             batch_size=self.batch_size,
-            show_progress_bar=self.show_bar
+            show_progress_bar=self.show_bar,
+            convert_to_numpy=True,
         )
 
-        embedding_dim = sent_vecs.size // (len(sents) * num_overlaps)
-        sent_vecs.resize(num_overlaps, len(sents), embedding_dim)
+        embedding_dim = sent_vecs.shape[-1]
+        sent_vecs = sent_vecs.reshape(num_overlaps, len(sents), embedding_dim)
 
         len_vecs = [len(line.encode("utf-8")) for line in overlaps]
-        len_vecs = np.array(len_vecs)
-        len_vecs.resize(num_overlaps, len(sents))
+        len_vecs = np.array(len_vecs).reshape(num_overlaps, len(sents))
 
         return sent_vecs, len_vecs
